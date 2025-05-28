a09f69ff7eece7250cd8e1c72e1ede94dac41be2
xostrch <xostrch@icloud.com>
2025-05-28 20:35:50 +0200

    skonczone

--- script.js
+++ script.js
@@ -25,23 +25,41 @@ const questions = [
     }
 ];
 
+
+
 let currentQuestionIndex = 0;
 const body = document.getElementById('body');
 let answered = false;
+let lastAnswer = false;
 
 
 function nextQuestion() {
     answered = false;
     currentQuestionIndex++; 
-    let result = `
+
+    let resultIdiot = `
     <div class="end-box">
         <div class="end-title">The End</div>
         <div class="end-result">Congratulatios! You are an idiot :(</div>
     </div>
     `;
-    if(currentQuestionIndex>3)
+
+    let resultNotIdiot = `
+    <div class="end-box">
+        <div class="end-title">The End</div>
+        <div class="end-result">Congratulatios! You are not an idiot :)</div>
+    </div>
+    `;
+
+    if(currentQuestionIndex>=questions.length)
     {
-        body.innerHTML=result;
+        if(lastAnswer===true)
+        {
+            body.innerHTML=resultIdiot
+        }else if(lastAnswer===false)
+        {
+            body.innerHTML=resultNotIdiot;
+        }
     }else
     {
         showQuiz()
@@ -57,10 +75,10 @@ function showQuiz(){
         <div class="quiz-title">The Idiot Test</div>
         <div class="quiz-question">`+currentQuestion.question+`</div>
         <div class="quiz-all-answers">
-            <div class="quiz-answer" class="quiz-answer" onclick="checkAnswer(0)">`+currentQuestion.answer[0]+`</div>
-            <div class="quiz-answer" class="quiz-answer" onclick="checkAnswer(1)">`+currentQuestion.answer[1]+`</div>
-            <div class="quiz-answer" class="quiz-answer" onclick="checkAnswer(2)">`+currentQuestion.answer[2]+`</div>
-            <div class="quiz-answer" class="quiz-answer" onclick="checkAnswer(3)">`+currentQuestion.answer[3]+`</div>
+            <div class="quiz-answer" onclick="checkAnswer(0)">`+currentQuestion.answer[0]+`</div>
+            <div class="quiz-answer" onclick="checkAnswer(1)">`+currentQuestion.answer[1]+`</div>
+            <div class="quiz-answer" onclick="checkAnswer(2)">`+currentQuestion.answer[2]+`</div>
+            <div class="quiz-answer" onclick="checkAnswer(3)">`+currentQuestion.answer[3]+`</div>
             <div class="quiz-box-button">
                 <button class="quiz-button" onclick="nextQuestion()">NEXT</button>
             </div>
@@ -75,18 +93,34 @@ function checkAnswer(selectedIndex){
     if (answered) return;
     answered = true;
 
+    let quizAnswers = document.getElementsByClassName('quiz-answer');
     const currentQuestion = questions[currentQuestionIndex];
-    if(selectedIndex===currentQuestion.correct)
-    {
-        alert("dobrze")
-    }else{
-        alert("zle")
+    
+    for (let i = 0; i < quizAnswers.length; i++) {
+        quizAnswers[i].style.pointerEvents = "none";
     }
 
-    let quizAnswers = document.getElementsByClassName('quiz-answer');
     for (let i = 0; i < quizAnswers.length; i++) {
-    quizAnswers[i].style.pointerEvents = "none";
-}
+        if(selectedIndex===currentQuestion.correct)
+        {   
+            quizAnswers[currentQuestion.correct].style.backgroundColor = "rgba(136, 255, 136, 0.534)";
+            quizAnswers[currentQuestion.correct].style.border = "1px solid rgb(136, 255, 136)";
+            if (currentQuestionIndex === 3) {
+                lastAnswer = true;
+                }  
+        }
+        else
+        {
+            quizAnswers[currentQuestion.correct].style.backgroundColor = "rgba(136, 255, 136, 0.534)";
+            quizAnswers[currentQuestion.correct].style.border = "1px solid rgb(136, 255, 136)";          
+
+            quizAnswers[selectedIndex].style.backgroundColor = "rgba(255, 136, 136, 0.53)";
+            quizAnswers[selectedIndex].style.border = "1px solid rgb(255, 136, 136)"; 
+            if (currentQuestionIndex === 3) {
+                lastAnswer = false;
+                }
+        }
+    }
 }
 
 
